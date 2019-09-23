5648dedb187137520f70f244db9ea646e8d1b9d8
Hailey J Gaylor <48165448+Hgaylor0220@users.noreply.github.com>
2019-09-23 14:12:27 -0700

    used the dictionary to loop in returns
    Added a recourses

--- riddles.cs
+++ riddles.cs
@@ -1,8 +1,42 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+
+class Riddles
 {
+  public static Dictionary<string, string> riddles = new Dictionary<string, string>(){{"bubble gum","What goes in hard, comes out soft, and you blow continuously?"},{"left hand","What can you hold in your right hand, but not in your left?"}};
+
+
   static void Main()
   {
+    Console.WriteLine(riddles["bubble gum"]);
+    string yourRiddleAnswer = Console.ReadLine();
+    if(yourRiddleAnswer == "bubble gum" || yourRiddleAnswer == "Bubble Gum" || yourRiddleAnswer == "Bubblegum")
+    {
+      Console.WriteLine("You right, you right! Keep going?['Y' for yes, 'N' for no.]");
+      string nextRiddle = Console.ReadLine();
+      if(nextRiddle == "y" || nextRiddle == "Y")
+      {
+        Console.WriteLine(riddles["left hand"]);
+        string nextRiddleAnswer = Console.ReadLine();
+        if(nextRiddleAnswer == "left hand" || nextRiddleAnswer == "Left Hand")
+        {
+          Console.WriteLine("You right, you right!, that is all.");
+        }
+            // else {
+            //   Console.WriteLine("Eaten! Try again");
+            // }
+
+
+        else{
+          Console.WriteLine("Eaten! Try again? ['Y' for yes, 'N' for no.]");
+          string nextRiddle1 = Console.ReadLine();
+          if(nextRiddle1 == "y" || nextRiddle1 == "Y")
+          {
+          Main();
+          }
+        }
+      }
+    }
   }
 }
